1c51f36b9555bcf408d7833f6fccac8abf7dd466
YugangLiao <459518627@qq.com>
2023-04-16 21:15:22 +0800

    change

--- src/main/java/com/nowcoder/community/service/AlphaService.java
+++ src/main/java/com/nowcoder/community/service/AlphaService.java
@@ -16,18 +16,18 @@ public class AlphaService {
     @Autowired
     private AlphaDao alphaDao;
 
-//    public AlphaService(){
-//        System.out.println("实例化AlphaService");
-//    }
-//
-//    @PostConstruct
-//    public void init(){
-//        System.out.println("初始化AlphaService");
-//    }
-//    @PreDestroy
-//    public void destory(){
-//        System.out.println("销毁AlphaService");
-//    }
+    public AlphaService(){
+        System.out.println("实例化AlphaService");
+    }
+
+    @PostConstruct
+    public void init(){
+        System.out.println("初始化AlphaService");
+    }
+    @PreDestroy
+    public void destory(){
+        System.out.println("销毁AlphaService");
+    }
      public String find(){
         return alphaDao.select();
     }
