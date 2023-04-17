92f3f9f1184f41a602dc2cddfd8ef2116bffedb2
YugangLiao <459518627@qq.com>
2023-04-17 11:52:57 +0800

    followservice

--- src/main/java/com/nowcoder/community/service/FollowService.java
+++ src/main/java/com/nowcoder/community/service/FollowService.java
@@ -1,8 +1,11 @@
 package com.nowcoder.community.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+
 public class FollowService {
-<<<<<<< HEAD
-=======
-    //
->>>>>>> c04467f (version 4.17)
+    @Autowired
+    private RedisTemplate redisTemplate ;
+
+
 }
